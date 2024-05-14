56a6cd309797b19b97c4a16a300b5874d49ad880
nicholasdavolt <nicholasdavolt@gmail.com>
2024-05-14 11:38:31 -0500

    Completed additional endpoints

--- main.go
+++ main.go
@@ -1,20 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 	mux := http.NewServeMux()
+	apiCFG := apiConfig{
+		fileserverHits: 0,
+	}
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
+	mux.Handle("/app/*", http.StripPrefix("/app", apiCFG.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCFG.handlerHits)
+	mux.HandleFunc("/reset", apiCFG.handlerMetricReset)
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func (cfg *apiConfig) handlerMetricReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Metrics reset to %v", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) handlerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
