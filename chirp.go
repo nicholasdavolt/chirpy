8fffa71a1a12a7a5e31bc9dc4e002d3f1f13da67
nicholasdavolt <nicholasdavolt@gmail.com>
2024-05-21 21:10:59 -0500

    Completed GET and POST for chirps

---
+++ chirp.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"sort"
+	"strings"
+)
+
+type Chirp struct {
+	Id   int    `json:"id"`
+	Body string `json:"body"`
+}
+
+func (cfg *apiConfig) handlerChirpReceive(w http.ResponseWriter, r *http.Request) {
+	type inputs struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	input := inputs{}
+	err := decoder.Decode(&input)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode input")
+		return
+
+	}
+
+	cleaned, err := validateChirp(input.Body)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	chirp, err := cfg.DB.CreateChirp(cleaned)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not Create Chirp")
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, chirp)
+}
+
+func (cfg *apiConfig) handlerChirpGet(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.DB.GetChirps()
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not retrieve Chirps")
+	}
+
+	chirps := []Chirp{}
+
+	for _, chirp := range dbChirps {
+		chirps = append(chirps, Chirp{
+			Id:   chirp.Id,
+			Body: chirp.Body,
+		})
+	}
+
+	sort.Slice(chirps, func(i, j int) bool {
+		return chirps[i].Id < chirps[j].Id
+	})
+
+	respondWithJSON(w, http.StatusOK, chirps)
+}
+
+func validateChirp(body string) (string, error) {
+	const maxChirpLength = 140
+
+	if len(body) > maxChirpLength {
+		return "", errors.New("Chirp is too long")
+	}
+
+	return cleanChirp(body), nil
+
+}
+
+func cleanChirp(input string) string {
+	replaceValues := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(input, " ")
+	replacement := "****"
+
+	for i, word := range words {
+		for _, val := range replaceValues {
+			if strings.ToLower(word) == val {
+				words[i] = replacement
+
+			}
+		}
+	}
+
+	return strings.Join(words, " ")
+
+}
--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+
+	database "github.com/nicholasdavolt/chirpy/internal"
 )
+
+type apiConfig struct {
+	fileserverHits int
+	DB             *database.DB
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+
+	db, err := database.NewDB("database.json")
+
+	if err != nil {
+		log.Printf("DB ERROR %s", err)
+	}
+
 	mux := http.NewServeMux()
 	apiCFG := apiConfig{
 		fileserverHits: 0,
+		DB:             db,
 	}
 
 	srv := &http.Server{
@@ -22,7 +37,8 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", apiCFG.handlerHits)
 	mux.HandleFunc("GET /api/reset", apiCFG.handlerMetricReset)
-	mux.HandleFunc("POST /api/validate_chirp", handlerChirpValidate)
+	mux.HandleFunc("POST /api/chirps", apiCFG.handlerChirpReceive)
+	mux.HandleFunc("GET /api/chirps", apiCFG.handlerChirpGet)
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
